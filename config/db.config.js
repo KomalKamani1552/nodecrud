273ae12a7b5fca0cdea9d95f007bd8ac0560d69c
KomalKamani1552 <97843807+KomalKamani1552@users.noreply.github.com>
2023-09-22 16:49:10 +0530

    third commit

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // url: "mongodb://127.0.0.1:27017/abc",
-     url: "mongodb://127.0.0.1:5000/abc",
+     url: "mongodb://172.20.48.1:5000/abc",
     TOKEN_SECRET :"thisissecretkeyforjwtauthorization",
     EXPIRY : 5000
   };
